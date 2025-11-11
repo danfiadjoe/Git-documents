aec3b6fe13e53b4d30ed2787c0ba52f683a2a4e9
Daniel Fiadjoe <danielfiadjoe@macbookair.home>
2025-11-11 18:24:13 -0330

    Commiting the 1st codes of the Hotel Booking and Reservation System

---
+++ Hotel_Booking_and_Reservation_System/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+app.get('/hotels', (req, res) => {
+  db.query('SELECT * FROM hotels', (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+app.listen(3001, () => console.log('Server running on port 3001'));
